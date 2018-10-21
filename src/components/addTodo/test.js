8d78a1e5d8bd05efce9cba47b606c0c6b5b344b5
Tatsuya Asami <sharekobekko@gmail.com>
2018-10-22 00:37:01 +0900

    add Adapter

--- src/components/addTodo/test.js
+++ src/components/addTodo/test.js
@@ -1,6 +1,10 @@
+/* global it, describe, expect */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import AddTodo from '.';
+
+configure({ adapter: new Adapter() });
 
 describe('AddTodo component', () => {
   it('Should render successfully', () => {

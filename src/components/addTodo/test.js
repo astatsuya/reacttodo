f6823eae97e961003fa7ff9c2b70473ae0a0a1ac
Tatsuya Asami <sharekobekko@gmail.com>
2018-10-22 11:44:43 +0900

    defined component and submitMock

--- src/components/addTodo/test.js
+++ src/components/addTodo/test.js
@@ -1,4 +1,4 @@
-/* global it, describe, expect, jest */
+/* global it, describe, expect, jest, beforeEach */
 import React from 'react';
 import { shallow, configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -7,25 +7,32 @@ import AddTodo from '.';
 configure({ adapter: new Adapter() });
 
 describe('AddTodo component', () => {
+  let component;
+  const submitMock = jest.fn();
+
+  beforeEach(() => {
+    component = shallow(
+      <AddTodo
+        submitTodo={submitMock}
+      />,
+    );
+  });
+
   it('Should render successfully', () => {
-    const component = shallow(<AddTodo />);
     expect(component.exists()).toEqual(true);
   });
 
   it('Should have one input', () => {
-    const component = shallow(<AddTodo />);
     expect(component.find('.todo-input').length).toEqual(1);
   });
 
   describe('Add todo button', () => {
     it('Should exist', () => {
-      const component = shallow(<AddTodo />);
       expect(component.find('.todo-submit').length).toEqual(1);
     });
 
     it('Should call the submitTodo function when clicked', () => {
-      const submitMock = jest.fn();
-      const component = mount(<AddTodo submitTodo={submitMock}) />);
+      component = mount(<AddTodo submitTodo={submitMock} />);
 
       expect(submitMock.mock.calls.length).toEqual(0);
       component.find('form').simulate('submit');

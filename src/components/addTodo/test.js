7ac35eedf7deb98316cf6dd511ba6b7638c16208
Tatsuya Asami <sharekobekko@gmail.com>
2018-10-22 11:16:22 +0900

    add submit function

--- src/components/addTodo/test.js
+++ src/components/addTodo/test.js
@@ -1,6 +1,6 @@
-/* global it, describe, expect */
+/* global it, describe, expect, jest */
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { shallow, configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import AddTodo from '.';
 
@@ -10,5 +10,26 @@ describe('AddTodo component', () => {
   it('Should render successfully', () => {
     const component = shallow(<AddTodo />);
     expect(component.exists()).toEqual(true);
+  });
+
+  it('Should have one input', () => {
+    const component = shallow(<AddTodo />);
+    expect(component.find('.todo-input').length).toEqual(1);
+  });
+
+  describe('Add todo button', () => {
+    it('Should exist', () => {
+      const component = shallow(<AddTodo />);
+      expect(component.find('.todo-submit').length).toEqual(1);
+    });
+
+    it('Should call the submitTodo function when clicked', () => {
+      const submitMock = jest.fn();
+      const component = mount(<AddTodo submitTodo={submitMock}) />);
+
+      expect(submitMock.mock.calls.length).toEqual(0);
+      component.find('form').simulate('submit');
+      expect(submitMock.mock.calls.length).toEqual(1);
+    });
   });
 });

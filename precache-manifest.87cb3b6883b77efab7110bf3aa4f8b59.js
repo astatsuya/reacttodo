b8297f2378e6c1850437de1859ba7a0f01c7c713
Tatsuya Asami <sharekobekko@gmail.com>
2018-10-23 19:38:28 +0900

    Updates

---
+++ precache-manifest.87cb3b6883b77efab7110bf3aa4f8b59.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8dfc5649fb196176564",
+    "url": "/reacttodo/static/js/runtime~main.b8dfc564.js"
+  },
+  {
+    "revision": "d501b3639cfa29bf7a43",
+    "url": "/reacttodo/static/js/main.d501b363.chunk.js"
+  },
+  {
+    "revision": "6d1d97d2a3511ca266f6",
+    "url": "/reacttodo/static/js/1.6d1d97d2.chunk.js"
+  },
+  {
+    "revision": "d3e22c0f7bf0c7537d65989163119d49",
+    "url": "/reacttodo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f651db3471e529533fcd19595628b38.js"
+  "/reacttodo/precache-manifest.87cb3b6883b77efab7110bf3aa4f8b59.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reacttodo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
